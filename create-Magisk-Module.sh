0542482b4074a1e4d92b541e7f5ae0b42e924746
rocket100 <78050149+rocket100@users.noreply.github.com>
2024-01-07 16:31:24 +0100

    Update create-Magisk-Module.sh

--- create-Magisk-Module.sh
+++ create-Magisk-Module.sh
@@ -3,7 +3,7 @@ cd Noto
 mkdir -p system/fonts
 mkdir -p META-INF/com/google/android
 wget https://github.com/dmlls/whatsapp-emoji-linux/releases/download/2.23.2.72-1/WhatsAppEmoji.ttf -O system/fonts/NotoColorEmoji.ttf
-wget https://github.com/topjohnwu/Magisk/blob/master/scripts/module_installer.sh -O META-INF/com/google/android/update-binary
+wget https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh -O META-INF/com/google/android/update-binary
 echo "#MAGISK" > META-INF/com/google/android/updater-script
 wget https://raw.githubusercontent.com/rocket100/WA-Android-System-Font/main/module.prop
 7z a WA.zip *
