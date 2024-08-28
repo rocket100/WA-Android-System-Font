a122ebf5fb615c9ad943bf9de1cd8241ea9f72ae
rocket100 <78050149+rocket100@users.noreply.github.com>
2024-08-28 02:34:03 +0200

    Update variable-size.sh

--- variable-size.sh
+++ variable-size.sh
@@ -3,8 +3,8 @@ PIXELSIZE=64
 git clone https://github.com/dmlls/whatsapp-emoji-linux
 
 cd ~/whatsapp-emoji-linux
-sed -i 's/160/$PIXELSIZE/g' Makefile
+sed -i "s/160/$PIXELSIZE/g" Makefile
 cd png
 mkdir $PIXELSIZE
-mogrify -resize $PIXELSIZEx$PIXELSIZE -path ./$PIXELSIZE 160/*.png
+#mogrify -resize $PIXELSIZEx$PIXELSIZE -path ./$PIXELSIZE 160/*.png
 
